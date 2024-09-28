8185515176e952f12ebe95dc167f5299b8a5ba59
Arbaz Karimi <arbazkarimi@gmail.com>
2024-09-28 15:37:07 +0530

    add footer and get to top btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'move1': {
+          '0%': { transform: 'translateY(0px)' },
+          '50%': { transform: 'translateY(-10px)' },
+          '100%': { transform: 'translateY(0px)' },
+        }
+      },
+      animation: {
+        'move1': 'move1 1.5s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
